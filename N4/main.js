84a35b8eaf437f3c2353c8b304625ac4fc453297
Nagoluk <51786414+Nagoluk@users.noreply.github.com>
2019-12-01 18:28:00 +0200

    I fixed ex4

--- N4/main.js
+++ N4/main.js
@@ -1,10 +1,17 @@
-let listOfWord = "WWW 44gg 55HH GGg www";
+let listOfWord = "WWW 44gg gg GG jj JJ LL gG l";
 listOfWord = listOfWord.replace(/!/g, "").replace(/,/g, "").replace(/-/g, "").replace(/:/g, "").replace(/;/g, "");
 
 let textArr = listOfWord.split(" ");
+let abbr = " "; //абривіатури
+let moreUpper = " "; //більше прописних
+let moreLower = " "; //більше рядкових
+let symbolsEqNum = " "; //кількість цифр дорівнює кількості знаків:
+let sameSymbols = " "; //зустрічаються однакові символи 
+let sameWords = " ";
 
 
-for(let a = 0; a < textArr.length; ++a){
+
+for(let a = 0; a < textArr.length ; ++a){
   let upper = 0, lower = 0, number = 0;
   let set = new Set();
    for(let b of textArr[a]){
@@ -20,32 +27,60 @@ for(let a = 0; a < textArr.length; ++a){
         if(48 <= b.charCodeAt() && b.charCodeAt() <= 57){
             ++number;
         }
-    }
-    if(upper > lower){
-      if(lower ==0 && number == 0){
-        console.log("Абривіатури: " +  textArr[a]); ;    
-        continue;  
-      }
 
-     console.log("Більше прописних: " +  textArr[a]);
         
     }
 
-    if(lower + upper == number){
-       console.log("Кількість цифр дорівнює кількості знаків: " + textArr[a]);
-    }
-
     if(set.size < textArr[a].length) {
-      console.log("Зустрічаються однакові символи " + textArr[a]);
+        sameSymbols  +=  " " + textArr[a];
     }
-    
+
+    if(lower ==0 && number == 0){
+        abbr += " " + textArr[a];    
+    }
+
+    if(upper > lower){
+      moreUpper += " " + textArr[a];
+    }
+
+  
+
+    if (upper < lower){
+      moreLower += " " + textArr[a];
+    }
+
+ 
+
+    if(lower + upper == number){
+      symbolsEqNum  +=  " " + textArr[a];
+    }
+ 
 }
 
-for(let a = 0; a < textArr.length; ++a){
-  for(let b = 1; b <  textArr.length; ++b){
-      if(textArr[a].toLowerCase() == textArr[b].toLowerCase()){
-        console.log(textArr[a] + " == " +  textArr[b])
-      }
-  }
+let shiftWord;
+while(true){
+
+  shiftWord = textArr.shift();
+
+  tempArray  = textArr.filter((item, index) => {
+      if(shiftWord.toUpperCase() == item.toUpperCase()) textArr = textArr.slice(index)
+      return shiftWord.toUpperCase() == item.toUpperCase();
+  });
+
+  if(tempArray.length!=0){
+      sameWords += " | " + shiftWord + ","+ tempArray + " |";
+  } 
+
+ if(textArr.length <= 1) break
 }
 
+
+console.log("-----------------------------------------------------------");
+console.log(listOfWord);
+console.log("Більше прописних: " +  moreUpper);
+console.log("Більше рядкових: " +  moreLower);
+console.log("Абривіатури: " +  abbr);
+console.log("Кількість цифр дорівнює кількості знаків:" + symbolsEqNum);
+console.log("Зустрічаються однакові символи " + sameSymbols);
+console.log("Зустрічаються однакові слова " + sameWords);
+console.log("-----------------------------------------------------------");
